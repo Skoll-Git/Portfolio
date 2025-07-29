df5358d143466c31f55a11b6e995a0706243f6c9
Sk <kevindaubresse@gmail.com>
2025-07-29 14:27:22 +0200

    commit correction 29/07

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ function Home() {
     <div id='Home' className="flex flex-col-reverse md:flex-row justify-center items-center md:my-32 my-10">
         <div className='flex flex-col'>
             <h1 className="text-3xl md:text-5xl font-bold text-center md:text-left mt-4 md:mt-0">Bonjour, <br />Je suis <span className="text-accent">Kévin "Skoll" Daubresse</span></h1>
-            <p className="my-4 text-md text-center md:text-left">Je suis un devoloppeur FRONT-END Junior,<br /> utilisant React,TailWindCSS.<br />Contactez-moi si vous avez besoin de mes services.</p>
+            <p className="my-4 text-md text-center md:text-left">Je suis un developpeur FRONT-END Junior,<br /> utilisant React,TailWindCSS.<br />Contactez-moi si vous avez besoin de mes services.</p>
             <a href="#Contact" className='btn btn-accent md:w-fit'>
                 <Mail className='w-5 h-5' />
                 Contactez-moi.
